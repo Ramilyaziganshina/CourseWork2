652d7a85a201b45f55bc91ac3d9953d5d508ac5d
Рамиля <bzzik@list.ru>
2023-04-04 19:49:50 +0300

    initial commit

---
+++ src/main/java/com/example/examtickets/ExamTicketsApplication.java
@@ -0,0 +1,13 @@
+package com.example.examtickets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamTicketsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamTicketsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/examtickets/ExamTicketsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.examtickets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamTicketsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
